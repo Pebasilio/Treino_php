af66c9b8e907cb2f3d816d259939be0fe45ba510
Pedro H. Basilio <156047668+Pebasilio@users.noreply.github.com>
2026-04-29 13:12:22 -0300

    Add user info variables and age check logic
    Added variables and logic to display user information.

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php 
 
-echo "<h1>Olá mundo</h1>";
-require_once('config.php');
+//Define as variaveis
+$nome = 'Pedro'; //String
+$idade = 25; //Integer
+$altura = 1.75; //Float
+$estudante = true; //Boolean
+$comidasFavoritas =["Cachorro Quente", "Pizza", "Hamburguer","Sushi" ]; //Array de Strings
+$comidasFavoritas[] = "Sorvete"; //Adicionando um valor ao Array
+$comidasFavoritas[1] = "Salada"; //Adicionando um valor ao Array no indice 1
+
+// Mostra o valor da variavel
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "<br>";
+echo "Altura: " . $altura . "<br>";
+echo "Estudante: " . $estudante . "<br>";
+echo "Comidas Favoritas:" . $comidasFavoritas[3] . "<br>"; 
+
+// Mostra o valor da variavel
+print_r($nome); 
+print_r($idade);
+print_r($altura);
+print_r($estudante);
+print_r($comidasFavoritas);
+
+
+//Usuario maior de idade ou menor de idade
+if ($idade >= 18) { 
+    echo "O " . $nome . " é maior de idade!<br>";
+} else {
+    echo "O " . $nome . " é menor de idade!<br>";
+}
 
 ?>
