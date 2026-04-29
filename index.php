46e4f82c1bc657f72ef6d3b0cfd32c075363a3c3
Pedro Henrique Basilio <pedro.basilio@cernn.com.br>
2026-04-29 13:46:18 -0300

    Atualiza exemplos iniciais de PHP

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php 
 
+
 //Define as variaveis
-$nome = 'Pedro'; //String
-$idade = 25; //Integer
-$altura = 1.75; //Float
+$nome = 'Pedro Henrique Basilio'; //String
+$idade = 20; //Integer
+$altura = 1.86; //Float
 $estudante = true; //Boolean
 $comidasFavoritas =["Cachorro Quente", "Pizza", "Hamburguer","Sushi" ]; //Array de Strings
 $comidasFavoritas[] = "Sorvete"; //Adicionando um valor ao Array
 $comidasFavoritas[1] = "Salada"; //Adicionando um valor ao Array no indice 1
+$comidasFavoritas[0] = "Lasanha"; //Adicionando um valor ao Array no indice 0
 
 // Mostra o valor da variavel
 echo "Nome: " . $nome . "<br>";
 echo "Idade: " . $idade . "<br>";
 echo "Altura: " . $altura . "<br>";
 echo "Estudante: " . $estudante . "<br>";
-echo "Comidas Favoritas:" . $comidasFavoritas[3] . "<br>"; 
+echo "Comidas Favoritas:" . $comidasFavoritas[4] . "<br>"; 
 
 // Mostra o valor da variavel
 print_r($nome); 
@@ -23,6 +25,10 @@ print_r($altura);
 print_r($estudante);
 print_r($comidasFavoritas);
 
+//Percorre o Array e mostra o valor
+foreach($comidasFavoritas as $comidas){ 
+    echo $comidas . "<br>";
+}
 
 //Usuario maior de idade ou menor de idade
 if ($idade >= 18) { 
